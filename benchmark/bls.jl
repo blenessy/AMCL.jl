4209672a94c282809ec5fbfd00d71f9e8871e432
Zalan Blenessy <zalan.blenessy@gmail.com>
2020-04-21 08:00:54 +0200

    Added some more benchmarks to benchmark/bls.jl

--- benchmark/bls.jl
+++ benchmark/bls.jl
@@ -16,18 +16,35 @@ msg = AMCL.octet(ones(UInt8, 200)) # constant 200 byte message
 
 for c in curves
     println("== BLS_$(c)_KEY_PAIR_GENERATE ==")
-    sk = AMCL.octet(eval(Meta.parse("AMCL.BGS_$(c)")))
-    # should fit BLS48
-    pk = AMCL.octet(16 * eval(Meta.parse("AMCL.BFS_$(c)")))
+    sk1 = AMCL.octet(eval(Meta.parse("AMCL.BGS_$(c)")))
+    sk2 = AMCL.octet(eval(Meta.parse("AMCL.BGS_$(c)")))
+    pk1 = AMCL.octet(16 * eval(Meta.parse("AMCL.BFS_$(c)"))) # should fit BLS48
+    pk2 = AMCL.octet(16 * eval(Meta.parse("AMCL.BFS_$(c)"))) # should fit BLS48
     api = eval(Meta.parse("AMCL.BLS_$(c)_KEY_PAIR_GENERATE"))
-    @btime $api(rng, $sk, $pk)
+    @assert api(rng, sk1, pk1) == AMCL.BLS_OK
+    @btime $api(rng, $sk2, $pk2);
 
     println("== BLS_$(c)_SIGN ==")
-    sig = AMCL.octet(eval(Meta.parse("AMCL.BFS_$(c)")) + 1)
+    sig1 = AMCL.octet(eval(Meta.parse("AMCL.BFS_$(c)")) + 1)
+    sig2 = AMCL.octet(eval(Meta.parse("AMCL.BFS_$(c)")) + 1)
     api = eval(Meta.parse("AMCL.BLS_$(c)_SIGN"))
-    @btime $api($sig, msg, $sk)
+    @assert api(sig1, msg, sk1) == AMCL.BLS_OK
+    @btime $api($sig2, msg, $sk2);
+
+    println("== BLS_$(c)_ADD_G1 ==")
+    sig = AMCL.octet(eval(Meta.parse("AMCL.BFS_$(c)")) + 1)
+    api = eval(Meta.parse("AMCL.BLS_$(c)_ADD_G1"))
+    @assert api(sig1, sig2, sig) == AMCL.BLS_OK
+    @btime $api($sig1, $sig2, $sig)
+
+    println("== BLS_$(c)_ADD_G2 ==")
+    pk = AMCL.octet(16 * eval(Meta.parse("AMCL.BFS_$(c)"))) # should fit BLS48
+    api = eval(Meta.parse("AMCL.BLS_$(c)_ADD_G2"))
+    @assert api(pk1, pk2, pk) == AMCL.BLS_OK
+    @btime $api($pk1, $pk2, $pk)
 
     println("== BLS_$(c)_VERIFY ==")
     api = eval(Meta.parse("AMCL.BLS_$(c)_VERIFY"))
-    @btime $api($sig, msg, $pk)
+    @assert api(sig, msg, pk) == AMCL.BLS_OK
+    @btime $api($sig, msg, $pk);
 end
